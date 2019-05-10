8ec45ea09c9341bc4a36946c8567d10e98693e89
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-05-10 10:42:42 +0530

    setup done

---
+++ sinhalaShoppingBackend/src/main/java/firewire/lk/sinhalaShoppingBackend/App.java
@@ -0,0 +1,13 @@
+package firewire.lk.sinhalaShoppingBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
